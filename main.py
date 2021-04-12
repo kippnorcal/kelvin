cba1b5bf89473f3970fb1b1ec6cdc9868a23189e
Susan Xiong <48801993+susanxiong@users.noreply.github.com>
2021-04-12 15:50:34 -0700

    Black auto-reformat

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ import requests
 import config
 from mailer import Mailer
 
+
 class Connector:
     """
-    Data connector for Extracting data, Transforming into dataframes, 
+    Data connector for Extracting data, Transforming into dataframes,
     ?? and loads into db (or keep the load separate from this class since we have sql_sorcery?)
     """
 
@@ -22,9 +23,11 @@ class Connector:
         token = config.API_TOKEN
         self.headers = {"Authorization": f"token {token}"}
         self.url = "https://pulse.kelvin.education/api/v1/pulse_responses"
-        
-    def get_last_dw_update(self): 
-        df = pd.read_sql_table('kelvin_pulse_responses', con=self.sql.engine, schema=self.sql.schema)
+
+    def get_last_dw_update(self):
+        df = pd.read_sql_table(
+            "kelvin_pulse_responses", con=self.sql.engine, schema=self.sql.schema
+        )
         return df["LastUpdated"].max()
 
     def get_responses(self):
@@ -33,53 +36,55 @@ class Connector:
         all_records = []
         while True:
             r = requests.get(f"{self.url}?page={page}", headers=self.headers).json()
-            if r: 
+            if r:
                 all_records.extend(r)
                 page = page + 1
             else:
                 break
-    
+
         return all_records
-    
-    def normalize_json(self, records): #do these variables need to be declared within the class?
+
+    def normalize_json(
+        self, records
+    ):  # do these variables need to be declared within the class?
         """
-           Takes in dataframe of all data to be processed, and list of record paths.
-           Loops over nested json and returns normalized data frame. 
+        Takes in dataframe of all data to be processed, and list of record paths.
+        Loops over nested json and returns normalized data frame.
         """
         df = pd.DataFrame()
         df = pd.json_normalize(
             records,
-            record_path = ["responses", "choices"],
-            meta = [
+            record_path=["responses", "choices"],
+            meta=[
                 "id",
                 "pulse_id",
-                "pulse_name", 
+                "pulse_name",
                 "pulse_window_id",
-                "pulse_window_number", 
+                "pulse_window_number",
                 "pulse_window_start_date",
                 "pulse_window_end_date",
                 "pulse_respondent_type",
-                "email", 
-                "state_id", 
-                "district_id", 
-                "display_id", 
-                "participant_id", 
+                "email",
+                "state_id",
+                "district_id",
+                "display_id",
+                "participant_id",
                 "responded_at",
-                "needs_assistance", 
-                "needs_assistance_asked", 
-                ["responses", "question_id"], 
-                ["responses", "skipped"], 
+                "needs_assistance",
+                "needs_assistance_asked",
+                ["responses", "question_id"],
+                ["responses", "skipped"],
                 ["responses", "stem"],
                 ["responses", "is_favorable"],
-                ["responses", "comment"], 
-                ["responses", "comment_share_name"], 
-            ], 
-            errors = 'ignore',
-            sep = '_',
+                ["responses", "comment"],
+                ["responses", "comment_share_name"],
+            ],
+            errors="ignore",
+            sep="_",
         )
 
         """ Lint Columns (timestamp, sort order, data types) """
-        df['LastUpdated'] = dt.datetime.now()
+        df["LastUpdated"] = dt.datetime.now()
 
         col_order = [
             "id",
@@ -110,23 +115,30 @@ class Connector:
         ]
         df = df[col_order]
 
-        numerics = ['pulse_id', 'pulse_window_id', 'pulse_window_number', 'state_id', 'display_id', 'responses_question_id']
-        for item in numerics: 
+        numerics = [
+            "pulse_id",
+            "pulse_window_id",
+            "pulse_window_number",
+            "state_id",
+            "display_id",
+            "responses_question_id",
+        ]
+        for item in numerics:
             df[item] = pd.to_numeric(df[item])
-        
-        dates = ['pulse_window_start_date', 'pulse_window_end_date']
-        for item in dates: 
-            df[item] = df[item].astype('datetime64[ns]')
 
-        return df 
+        dates = ["pulse_window_start_date", "pulse_window_end_date"]
+        for item in dates:
+            df[item] = df[item].astype("datetime64[ns]")
 
-    def load_into_dw(self, df): 
+        return df
+
+    def load_into_dw(self, df):
         """Writes the data into the related table"""
-        
+
         tablename = "kelvin_pulse_responses"
         logging.debug(f"{tablename}: inserting {len(df)} records into {tablename}.")
-        self.sql.insert_into(tablename, df, chunksize=10000, if_exists='replace')
-    
+        self.sql.insert_into(tablename, df, chunksize=10000, if_exists="replace")
+
     def load_into_survey_model(self):
         """Take raw Kelvin data and parse it in to relational survey model dims and fact."""
         # This list allows us to loop through the table names in a specific order,
@@ -146,6 +158,7 @@ class Connector:
             self.sql.insert_into(table_name, df)
             logging.info(f"Inserted {len(df)} records into {table_name}.")
 
+
 def main():
 
     config.set_logging()
@@ -156,6 +169,7 @@ def main():
     connector.load_into_dw(df_trans)
 
     connector.load_into_survey_model()
+
 
 if __name__ == "__main__":
     try:
