7e35abca0a71f1109582f6df6b12b30a1166729f
Mark Young <markyoung.ca@gmail.com>
2024-01-31 14:50:52 -0800

    Refactored arg parser setup; added --truncate-reload arg

--- config.py
+++ config.py
@@ -3,10 +3,23 @@ import logging
 import os
 import sys
 
+def setup_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--debug", 
+        dest="debug",
+        help="Enable debug logging", 
+        action="store_true"
+        )
+    parser.add_argument(
+        "--truncate-reload", 
+        dest="truncate_reload", 
+        help="Full truncate and reload of Kevin Pulse data", 
+        action="store_true"
+        )
+    return parser.parse_args()
 
-parser = argparse.ArgumentParser(description="Pick which ones")
-parser.add_argument("--debug", help="Enable debug logging", action="store_true")
-ARGS, _ = parser.parse_known_args()
+ARGS = setup_arg_parser()
 
 ENABLE_MAILER = int(os.getenv("ENABLE_MAILER", default=0))
 DEBUG = ARGS.debug or int(os.getenv("DEBUG", default=0))
